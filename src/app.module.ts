b1f5d01d39626c90487ff1cf9fe5e889ec0a95e8
Ejike Odoh <ejike.developer@gmail.com>
2026-09-15 04:47:25 +0100

    15th sept

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,21 +11,21 @@ import { CbcStudentsModule } from './cbc-students/cbc-students.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (configService: ConfigService) => ({
-    //     type: 'postgres',
-    //     host: configService.getOrThrow<string>('DB_HOST'),
-    //     port: configService.getOrThrow<number>('DB_PORT'),
-    //     database: configService.getOrThrow<string>('DB_DATABASE'),
-    //     username: configService.getOrThrow<string>('DB_USERNAME'),
-    //     password: configService.getOrThrow<string>('DB_PASSWORD'),
-    //     autoLoadEntities: true,
-    //     synchronize: true,
-    //   }),
-    // }),
-    // SchoolsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow<string>('DB_HOST'),
+        port: configService.getOrThrow<number>('DB_PORT'),
+        database: configService.getOrThrow<string>('DB_DATABASE'),
+        username: configService.getOrThrow<string>('DB_USERNAME'),
+        password: configService.getOrThrow<string>('DB_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    SchoolsModule,
     CbcStudentsModule
   ],
   controllers: [AppController],
